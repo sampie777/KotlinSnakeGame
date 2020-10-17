f34125b2adfc0085af43efac031220beed2d084b
S. Jansen <sa.jansen1@gmail.com>
2020-10-17 10:01:24 +0200

    Updated SoundPlayer to better use of newer libraries

--- src/main/kotlin/nl/sajansen/kotlinsnakegame/objects/sound/SoundPlayer.kt
+++ src/main/kotlin/nl/sajansen/kotlinsnakegame/objects/sound/SoundPlayer.kt
@@ -1,26 +1,32 @@
 package nl.sajansen.kotlinsnakegame.objects.sound
 
 import nl.sajansen.kotlinsnakegame.ApplicationInfo
-import sun.audio.AudioPlayer
-import sun.audio.AudioStream
 import java.io.File
-import java.io.FileInputStream
 import java.util.logging.Logger
+import javax.sound.sampled.AudioInputStream
+import javax.sound.sampled.AudioSystem
+import javax.sound.sampled.Clip
+import javax.sound.sampled.DataLine
+
 
 object SoundPlayer {
     private val logger = Logger.getLogger(SoundPlayer::class.java.name)
 
     fun play(sound: Sounds) {
         val file = File(ApplicationInfo::class.java.classLoader.getResource(sound.path)!!.file)
+        logger.info("Playing sound: ${file.absolutePath}")
+
         if (!file.exists()) {
             logger.warning("Sound file not found: ${file.absolutePath}")
         }
 
-        val inputStream = FileInputStream(file)
-
         try {
-            val audioStream = AudioStream(inputStream)
-            AudioPlayer.player.start(audioStream)
+            val inputStream: AudioInputStream = AudioSystem.getAudioInputStream(file)
+            val dataLineInfo = DataLine.Info(Clip::class.java, inputStream.format)
+
+            val clip = AudioSystem.getLine(dataLineInfo) as Clip
+            clip.open(inputStream)
+            clip.start()
         } catch (e: Exception) {
             logger.severe("Failed to play sound: ${file.absolutePath}")
             e.printStackTrace()
