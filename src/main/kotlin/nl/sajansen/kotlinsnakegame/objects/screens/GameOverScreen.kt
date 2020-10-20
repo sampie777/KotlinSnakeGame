6fed98eab633882302dda391a1ca56b59f27563b
S. Jansen <sa.jansen1@gmail.com>
2020-10-20 13:45:13 +0200

    Display player scores on game over screen

--- src/main/kotlin/nl/sajansen/kotlinsnakegame/objects/screens/GameOverScreen.kt
+++ src/main/kotlin/nl/sajansen/kotlinsnakegame/objects/screens/GameOverScreen.kt
@@ -3,6 +3,7 @@ package nl.sajansen.kotlinsnakegame.objects.screens
 
 import nl.sajansen.kotlinsnakegame.config.Config
 import nl.sajansen.kotlinsnakegame.objects.game.Game
+import nl.sajansen.kotlinsnakegame.objects.player.Player
 import nl.sajansen.kotlinsnakegame.objects.screens.drawableComponents.ComponentAlignment
 import nl.sajansen.kotlinsnakegame.objects.screens.drawableComponents.Label
 import java.awt.Color
@@ -16,26 +17,71 @@ object GameOverScreen : Screen() {
     override var backgroundOpacity = 200
     override var paintAsOverlay = true
 
-    private val messageLabel = Label()
-
     init {
+        initGui()
+    }
+
+    fun rebuildGui() {
+        components.clear()
+        initGui()
+    }
+
+    private fun initGui() {
         val titleLabel = Label(Config.gameOverMessage)
-        titleLabel.font = Font("Dialog", Font.PLAIN, 30)
+        titleLabel.font = Font("Dialog", Font.BOLD, 46)
+        titleLabel.position = Point(0, 80)
         titleLabel.componentAlignmentX = ComponentAlignment.CENTER
-        titleLabel.componentAlignmentY = ComponentAlignment.CENTER
         add(titleLabel)
 
-        messageLabel.font = Font("Dialog", Font.PLAIN, 22)
+        val messageLabel = Label(Game.deathMessage)
+        messageLabel.font = Font("Dialog", Font.ITALIC, 22)
         messageLabel.fontColor = Color(230, 230, 230)
+        messageLabel.position = Point(0, 140)
         messageLabel.componentAlignmentX = ComponentAlignment.CENTER
-        messageLabel.position = Point(0, Game.board.windowSize.height / 2 + 25)
         add(messageLabel)
+
+        showScores(Point(300, 250))
 
         GameOverlay.addControlsOverlay(this)
     }
 
+    private fun showScores(position: Point) {
+        val rowVerticalMargin = 45
+
+        val label = Label("SCORES")
+        label.font = Font("Dialog", Font.BOLD, 12)
+        label.position = position
+        label.componentAlignmentX = ComponentAlignment.CENTER
+        label.fontColor = Color(173, 173, 173)
+        components.add(label)
+
+        Game.players
+            .sortedBy { it.name }
+            .sortedByDescending { it.score }
+            .forEachIndexed { index, player ->
+                paintScore(
+                    Point(position.x, position.y + 30 + index * rowVerticalMargin),
+                    player
+                )
+            }
+    }
+
+    private fun paintScore(position: Point, player: Player) {
+        val nameLabel = Label(player.name)
+        nameLabel.position = position
+        nameLabel.font = Font("Dialog", Font.PLAIN, 24)
+        nameLabel.fontColor = Color(217, 217, 217)
+        components.add(nameLabel)
+
+        val scoreLabel = Label(player.score.toString())
+        scoreLabel.position = Point(position.x + 250, position.y)
+        scoreLabel.font = Font("Dialog", Font.BOLD, 24)
+        scoreLabel.fontColor = Color(217, 217, 217)
+        components.add(scoreLabel)
+    }
+
     override fun show(index: Int?) {
         super.show(index)
-        messageLabel.text = Game.deathMessage
+        rebuildGui()
     }
 }
